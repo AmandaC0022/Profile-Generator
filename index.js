d2e63bd84b25f4d383b5df519537549850772cd8
Amanda Morgan <AmandaC0022@gmail.com>
2021-08-29 14:52:37 -0500

    bug data not populating to index.html

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const render = require('./src/page-template'); 
 const fs = require("fs"); 
 const path = require("path"); 
+const Manager = require("./lib/Manager"); 
+const Engineer = require("./lib/Engineer"); 
+const Intern = require("./lib/Intern"); 
+const { getMaxListeners } = require('process');
 
 //this creates a path to the dist folder / __dirname is a node method 
 const OUTPUT_DIR = path.resolve(__dirname, "dist"); 
@@ -10,25 +14,12 @@ const outputPath = path.join(OUTPUT_DIR, "index.html");
 
 //We are hard coding this right now. This will be generated later from the command line prompts with inquirer
 const team = [ 
-    {
-        name: 'Amanda', 
-        id: 1, 
-        email: 'AmandaC0022@gmail.com', 
-        role: 'manager',
-    }, 
-    {
-        name: 'Ben', 
-        id: 2, 
-        email: 'Ben02@gmail.com', 
-        role: 'engineer',
-    }, 
-    {
-        name: 'Hannah', 
-        id: 3, 
-        email: 'Hannah@gmail.com', 
-        role: 'intern',
-    }
+    new Manager("Amanda", 1, "AmandaC0022@gmail.com"), 
+    new Engineer("Ben", 2, "Ben02@gmail.com"),
+    new Intern ("Hannah", 3, "Hannah@school.com"), 
+    new Engineer("Chris", 4, "Chrisipoo@yahoo.com") 
 ]
+
 //this writes the code from render(team) to the index.html file 
 fs.writeFileSync(outputPath, render(team), 'utf-8'); 
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,31 +1,31 @@
 const generateTeam = (team) => {
     const generateManager = (manager) => {
         return `
-        <h2 class="card-title">${manager.name}</h2>
+        <h2 class="card-title">${manager.getName()}</h2>
         <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li> 
         </ul>
         `;
-        //this is from the original mock up: 
-        // <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
     };
     const generateEngineer = (engineer) => {
         return `
-        <h2 class="card-title">${engineer.name}</h2>
+        <h2 class="card-title">${engineer.getName()}</h2>
         <ul class="list-group">
-                <li class="list-group-item">ID: ${engineer.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: ${engineer.getGithub()}</li> 
         </ul>
         `;
     }; 
-
     const generateIntern = (intern) => {
         return `
-        <h2 class="card-title">${intern.name}</h2>
+        <h2 class="card-title">${intern.getName()}</h2>
         <ul class="list-group">
-                <li class="list-group-item">ID: ${intern.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li> 
         </ul>
         `;
     }; 
@@ -35,7 +35,7 @@ const generateTeam = (team) => {
     htmlArr.push(
         // this grabs just those with the role of manager 
         team
-            .filter((employee) => employee.role === 'manager') 
+            .filter((employee) => employee.role === 'Manager') 
             //this goes over each index in the array and runs generateManager on each ith element or manager
             .map((manager) => generateManager(manager)) 
             .join("")
@@ -44,7 +44,7 @@ const generateTeam = (team) => {
     htmlArr.push(
         // this grabs just those with the role of engineer 
         team
-            .filter((employee) => employee.role === 'engineer') 
+            .filter((employee) => employee.role === 'Engineer') 
             .map((engineer) => generateEngineer(engineer)) 
             .join("")
     ); 
@@ -52,7 +52,7 @@ const generateTeam = (team) => {
     htmlArr.push(
         // this grabs just those with the role of manager 
         team
-            .filter((employee) => employee.role === 'intern') 
+            .filter((employee) => employee.role === 'Intern') 
             //this goes over each index in the array and runs generateManager on each ith element or manager
             .map((intern) => generateIntern(intern)) 
             .join("")
